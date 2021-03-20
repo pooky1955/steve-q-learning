36a4dd5e063652c375b210dd7f4c933ca7c763d6
James Liang <james.liang.cje@icloud.com>
2021-03-20 19:49:06 -0400

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from processing_py import *
-
+WIDTH = 600
+HEIGHT = 600
 class Player:
     JUMP_SPEED = 30
     JUMP_VEC = np.array([0,-JUMP_SPEED])
@@ -38,7 +39,7 @@ class Game:
     def __init__():
         pass
 
-app = App(600,400) # create window: width, height
+app = App(WIDTH,HEIGHT) # create window: width, height
 app.background(0,0,0) # set background:  red, green, blue
 app.fill(255,255,0) # set color for objects: red, green, blue
 app.rect(100,100,200,100) # draw a rectangle: x0, y0, size_x, size_y
