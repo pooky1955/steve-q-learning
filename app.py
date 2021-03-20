8f160da2967eb0253e8d2e03ed1ded0473f86552
James Liang <james.liang.cje@icloud.com>
2021-03-20 19:52:01 -0400

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from processing_py import *
 WIDTH = 600
 HEIGHT = 600
@@ -52,9 +53,6 @@ class Game:
 
 
 app = App(WIDTH,HEIGHT) # create window: width, height
-app.background(0,0,0) # set background:  red, green, blue
-app.fill(255,255,0) # set color for objects: red, green, blue
-app.rect(100,100,200,100) # draw a rectangle: x0, y0, size_x, size_y
-app.fill(0,0,255) # set color for objects: red, green, blue
-app.ellipse(300,200,50,50) # draw a circle: center_x, center_y, size_x, size_y
-app.redraw() # refresh the window
+game = Game()
+while True:
+    time.sleep(1)
