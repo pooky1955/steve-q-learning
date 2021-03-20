bd171295942d742e057a850d7ab04544c27624dd
James Liang <james.liang.cje@icloud.com>
2021-03-20 19:47:26 -0400

    update

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ from processing_py import *
 class Player:
     JUMP_SPEED = 30
     JUMP_VEC = np.array([0,-JUMP_SPEED])
+    PLAYER_WIDTH = 30
+    PLAYER_HEIGHT = 30
+    
     def __init__(self,x,y):
         self.pos = np.array([x,y])
+
     def jump(self):
         self.pos += JUMP_VEC
+        
+    def display(app):
+        app.rect(self.pos[0],self.pos[1],PLAYER_WIDTH,PLAYER_HEIGHT)
 
     
     
