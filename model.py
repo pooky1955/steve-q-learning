fbb66f8bfca2a725592bfb607e97f34dea41f631
James Liang <james.liang.cje@icloud.com>
2021-03-20 21:17:34 -0400

    update

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 from keras.layers import *
+from keras.optimizers import Adam
 from keras.models import Model
+
 def create_model():
     in_layer = Input(input_shape=(1))
     out_layer = Dense(1,activation="sigmoid")(in_layer)
     model = Model(in_layer,out_layer)
-    model.compile(optimizer="adam",loss="binary_crossentropy")
+    opt = Adam(lr=1e-5)
+    model.compile(optimizer=opt,loss="binary_crossentropy")
     return model
 
 
